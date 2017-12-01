026b50ffdf85cd75d6349ca4b06b33ecf3db9326
ssharm25 <ssharm25@ncsu.edu>
2017-12-01 01:12:51 -0500

    Update create-stream.py

--- Python Files/create-stream.py
+++ Python Files/create-stream.py
@@ -1,9 +1,10 @@
 # Simple script to create a kinesis stream
 # create-stream.py
 import boto3
+shardsNum = raw_input("Number of shards:")
 
 client = boto3.client('kinesis')
 response = client.create_stream(
    StreamName='twitter',
-   ShardCount=1
+   ShardCount=shardsNum
 )
