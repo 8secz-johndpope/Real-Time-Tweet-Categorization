609929b15969a12fab9eb5421b12e851fc70696e
sspatil4 <sspatil4@ncsu.edu>
2017-12-03 20:33:40 -0500

    Added elasticsearch configs

--- Python Files/config.py
+++ Python Files/config.py
@@ -1,2 +1,6 @@
 shardCount = 4
 scalingType = 'UNIFORM_SCALING'
+
+es_host = 'https://search-es-twitter-demo-rl75vbbrmigsqfakvv5od6cj4y.us-east-1.es.amazonaws.com'
+es_port = 80
+es_bulk_chunk_size = 1000
